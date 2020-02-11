06890df5090735c8386cd53a23936223724b25d7
Enkhnaran <enaa.mongolia@gmail.com>
2020-02-11 13:35:37 +0000

    typo fixed

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactMapGL from 'react-map-gl';
-import mapToken from 'src/components/Map/tokens.js';
+import mapToken from './tokens';
 
 class Map extends Component {
 
