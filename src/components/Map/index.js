781f43e584f6ac1fc5440a2b8c450991255d3832
Karen <karenbrady92@hotmail.co.uk>
2020-02-11 16:36:41 +0000

    edited map style

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import ReactMapGL from 'react-map-gl';
+import mapToken from './token';
 
-class Map extends Component {
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    width: "98vw",
+    height: "98vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 1.2
+  })
 
-  state = {
-    viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
-    }
-  };
-
-  render() {
     return (
       <ReactMapGL
-        {...this.state.viewport}
-        mapboxApiAccessToken={''}
-        onViewportChange={(viewport) => this.setState({viewport})}
-      />
+        {...viewport}
+        mapboxApiAccessToken={mapToken.publicToken}
+        mapStyle="mapbox://styles/kb51/ck6hzqrra127k1imp75s9nivk"
+        onViewportChange={viewport => {
+          setViewport(viewport)
+        }}
+      >
+        markers here
+        </ReactMapGL>
     );
   }
-}
-
-export default Map;
