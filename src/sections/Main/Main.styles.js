87099c856749d3f15b0c289ac89da66b5bf7092a
Karen <karenbrady92@hotmail.co.uk>
2020-02-14 13:22:44 +0000

    tidied up styling

--- src/sections/Main/Main.styles.js
+++ src/sections/Main/Main.styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 85vh;
+  height: 100vh;
   display: flex;
   justify-content: space-between;
   padding: 20px;
