613018a561c17ee5311f97704275166077f0a3ae
Karen <karenbrady92@hotmail.co.uk>
2020-02-15 20:14:48 +0000

    added Add Destination modal form fields

--- src/sections/Main/components/Modals/Modals.js
+++ src/sections/Main/components/Modals/Modals.js
@@ -1,18 +1,59 @@
 import React from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Image, Modal, Form, Input, Icon } from 'semantic-ui-react'
 
 
 
 const AddDestination = (props) => (
   <Modal trigger={props.triggerButton}>
     <Modal.Header>Add a Destnation</Modal.Header>
-    <Modal.Content image>
-      <Image wrapped size='medium' src='https://react.semantic-ui.com/images/avatar/large/rachel.png' />
+    <Modal.Content>
       <Modal.Description>
         <Header>Add a Destination</Header>
-        <p>
-          This is a modal.
-        </p>
+        <Form>
+          <Form.Group widths='equal'>
+            <div>
+              <Input list='countries' placeholder='Choose Country...' />
+              <datalist id='countries'>
+                <option value='English' />
+                <option value='Chinese' />
+                <option value='Dutch' />
+              </datalist>
+            </div>
+          </Form.Group>
+          <Form.Group grouped>
+            <Form.Field
+              label='I Have Been Here'
+              control='input'
+              type='radio'
+              name='htmlRadios'
+            />
+            <Form.Field
+              label='I Want To Go Here'
+              control='input'
+              type='radio'
+              name='htmlRadios'
+            />
+          </Form.Group>
+          <Form.Group grouped>
+            <Form.Field label='Add a Fun Fact' control='input' type='checkbox' />
+          </Form.Group>
+          <Form.Field label='Add Notes' control='textarea' rows='3' />
+          <Form.Field>
+          <Button animated>
+              <Button.Content visible>Add a Photo</Button.Content>
+              <Button.Content hidden>
+                <Icon name='arrow right' />
+              </Button.Content>
+            </Button>          </Form.Field>
+          <Form.Field>
+            <Button animated>
+              <Button.Content visible>Submit</Button.Content>
+              <Button.Content hidden>
+                <Icon name='arrow right' />
+              </Button.Content>
+            </Button>
+          </Form.Field>
+        </Form>
       </Modal.Description>
     </Modal.Content>
   </Modal>
