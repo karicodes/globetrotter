a8a56ebc88bb328652ff6145db66db007a5c5932
Karen <karenbrady92@hotmail.co.uk>
2020-02-16 16:56:05 +0000

    implemented global state with context and used it in the country drop down on Add Destination modal

--- src/sections/Main/Main.js
+++ src/sections/Main/Main.js
@@ -1,44 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Map from './components/Map';
 import List from './components/List';
 import Summary from './components/Summary';
 import ToggleButtons from './components/ToggleButtons';
 import { Container, Content } from './Main.styles';
+import { CountryProvider } from './CountryContext';
 
 function Main() {
-  const [items, setItems] = useState([]);
-  const [isLoaded, setIsLoaded] = useState(false);
+
   const [view, setView] = useState('map');
 
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-      .then(res => res.json())
-      .then(json => {
-        setItems(json);
-        setIsLoaded(true);
-      })
-  }, [])
-
-  if (!isLoaded) {
-    return <p>Loading...</p>
-  }
-
   return (
-    <Container>
-      <ToggleButtons view={view} setView={setView} />
-      <Content>
-        {view === 'map' && <Map />}
-        {view === 'list' && <List />}
-        {view === 'summary' && <Summary />}
-      </Content>
-      {/* <select>
-        {items.map(item => (
-          <option key={item.id}>
-            {item.name}
-          </option>
-        ))}
-      </select> */}
-    </Container>
+    <CountryProvider>
+      <Container>
+        <ToggleButtons view={view} setView={setView} />
+        <Content>
+          {view === 'map' && <Map />}
+          {view === 'list' && <List />}
+          {view === 'summary' && <Summary />}
+        </Content>
+      </Container>
+    </CountryProvider>
+
   );
 }
 
--- src/sections/Main/components/Modals/Modals.js
+++ src/sections/Main/components/Modals/Modals.js
@@ -1,57 +1,63 @@
-import React from 'react'
-import { Button, Header, Modal, Form, Input, Icon } from 'semantic-ui-react'
+import React, { useContext } from 'react';
+import { Button, Modal, Form, Input, Icon } from 'semantic-ui-react';
+import { CountryContext } from '../../CountryContext';
 
-const AddDestination = (props) => (
-  <Modal trigger={props.triggerButton}>
-    <Modal.Header>Add a Destnation</Modal.Header>
-    <Modal.Content>
-      <Modal.Description>
-        <Form>
-          <Form.Group widths='equal'>
-            <div>
-              <Input list='countries' placeholder='Choose Country...' />
-              <datalist id='countries'>
-                <option value='Argentina' />
-                <option value='Andorra' />
-                <option value='Australia' />
-              </datalist>
-            </div>
-          </Form.Group>
-          <Form.Group grouped>
-            <Form.Field
-              label='I Have Been Here'
-              control='input'
-              type='radio'
-              name='htmlRadios'
-            />
-            <Form.Field
-              label='I Want To Go Here'
-              control='input'
-              type='radio'
-              name='htmlRadios'
-            />
-          </Form.Group>
-          <Form.Field>
-            <Button animated>
-              <Button.Content visible>Add a Photo</Button.Content>
-              <Button.Content hidden>
-                <Icon name='photo' />
-              </Button.Content>
-            </Button>
-          </Form.Field>
-          <Form.Field label='Add Notes' control='textarea' rows='3' />
-          <Form.Field>
-            <Button animated>
-              <Button.Content visible>Submit</Button.Content>
-              <Button.Content hidden>
-                <Icon name='arrow right' />
-              </Button.Content>
-            </Button>
-          </Form.Field>
-        </Form>
-      </Modal.Description>
-    </Modal.Content>
-  </Modal>
-)
+function AddDestination(props) {
+  const [countries, setCountries] = useContext(CountryContext)
+  return (
+    <Modal trigger={props.triggerButton}>
+      <Modal.Header>Add a Destnation</Modal.Header>
+      <Modal.Content>
+        <Modal.Description>
+          <Form>
+            <Form.Group widths='equal'>
+              <div>
+                <Input list='countries' placeholder='Choose Country...' />
+                <datalist id='countries'>
+                  {countries.map(item => (
+                    <option key={item.id}>
+                      {item.name}
+                    </option>
+                  ))}
+                </datalist>
+              </div>
+            </Form.Group>
+            <Form.Group grouped>
+              <Form.Field
+                label='I Have Been Here'
+                control='input'
+                type='radio'
+                name='htmlRadios'
+              />
+              <Form.Field
+                label='I Want To Go Here'
+                control='input'
+                type='radio'
+                name='htmlRadios'
+              />
+            </Form.Group>
+            <Form.Field>
+              <Button animated>
+                <Button.Content visible>Add a Photo</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='photo' />
+                </Button.Content>
+              </Button>
+            </Form.Field>
+            <Form.Field label='Add Notes' control='textarea' rows='3' />
+            <Form.Field>
+              <Button animated>
+                <Button.Content visible>Submit</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='arrow right' />
+                </Button.Content>
+              </Button>
+            </Form.Field>
+          </Form>
+        </Modal.Description>
+      </Modal.Content>
+    </Modal>
+  )
+}
 
 export default AddDestination;
