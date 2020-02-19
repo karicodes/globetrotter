d6fa7a099e188ef4033fad6121a4446ee1054024
Karen <karenbrady92@hotmail.co.uk>
2020-02-19 23:52:40 +0000

    added delete and edit buttons to pop ups

--- src/sections/Main/components/Map/PinInfo.js
+++ src/sections/Main/components/Map/PinInfo.js
@@ -1,21 +1,26 @@
-import React, {PureComponent} from 'react';
+import React, { PureComponent } from 'react';
 import { Container } from './Map.styles';
+import { Button } from 'semantic-ui-react'
+
 
 export default class CityInfo extends PureComponent {
   render() {
-    const {info} = this.props;
+    const { info } = this.props;
     const displayName = `${info.country.name}, ${info.country.region}`;
 
     return (
       <div>
         <Container>
           <span>
-          {displayName}
+            {displayName}
           </span>
           <img src={info.country.flag} width={20} alt='flag' />
         </Container>
-          <p>{info.notes}</p>
+        <p>{info.notes}</p>
+
         <img width={240} src={info.image} />
+        <Button icon='edit' />
+        <Button icon='delete' />
       </div>
     );
   }
