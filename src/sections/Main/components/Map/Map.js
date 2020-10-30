9186e673e326618a5f1bbdd9ad6e547b089acc42
kb51 <karenbrady92@hotmail.co.uk>
2020-10-30 09:05:39 +0000

    update package-lock

--- src/sections/Main/components/Map/Map.js
+++ src/sections/Main/components/Map/Map.js
@@ -1,5 +1,10 @@
 import React, { useContext, useState } from 'react';
-import MapGL, { Popup, NavigationControl, FullscreenControl, ScaleControl } from 'react-map-gl';
+import MapGL, {
+  Popup,
+  NavigationControl,
+  FullscreenControl,
+  ScaleControl,
+} from 'react-map-gl';
 import Pins from './Pins';
 import PinInfo from './PinInfo';
 import { MapContext } from '../../Contexts/MapContext';
@@ -8,38 +13,38 @@ const fullscreenControlStyle = {
   position: 'absolute',
   top: 0,
   left: 0,
-  padding: '10px'
+  padding: '10px',
 };
 
 const navStyle = {
   position: 'absolute',
   top: 36,
   left: 0,
-  padding: '10px'
+  padding: '10px',
 };
 
 const scaleControlStyle = {
   position: 'absolute',
   bottom: 36,
   left: 0,
-  padding: '10px'
+  padding: '10px',
 };
 
 export default function Map() {
   const [viewport, setViewport] = useState({
     latitude: 51.4934,
-    longitude: -0.000500,
+    longitude: -0.0005,
     zoom: 1,
     bearing: 0,
     pitch: 0,
-    width: "100%",
-    height: "100%",
-  })
+    width: '100%',
+    height: '100%',
+  });
 
   const [popupInfo, setPopUpInfo] = useState(null);
 
-  const _onClickMarker = country => {
-    setPopUpInfo(country)
+  const _onClickMarker = (country) => {
+    setPopUpInfo(country);
   };
 
   const mapContext = useContext(MapContext);
@@ -51,14 +56,21 @@ export default function Map() {
     <MapGL
       {...viewport}
       mapStyle="mapbox://styles/mapbox/dark-v9"
-      onViewportChange={viewport => {
-        setViewport(viewport)
+      onViewportChange={(viewport) => {
+        setViewport(viewport);
       }}
       mapboxApiAccessToken={process.env.REACT_APP_MAP_TOKEN}
     >
-
-      <Pins color='limegreen' data={visitedCountries} onClick={_onClickMarker} />
-      <Pins color='dodgerblue' data={bucketlistCountries} onClick={_onClickMarker} />
+      <Pins
+        color="limegreen"
+        data={visitedCountries}
+        onClick={_onClickMarker}
+      />
+      <Pins
+        color="dodgerblue"
+        data={bucketlistCountries}
+        onClick={_onClickMarker}
+      />
 
       {popupInfo ? (
         <Popup
@@ -75,7 +87,8 @@ export default function Map() {
             setVisitedCountries={setVisitedCountries}
             bucketlistCountries={bucketlistCountries}
             setBucketlistCountries={setBucketlistCountries}
-            info={popupInfo} />
+            info={popupInfo}
+          />
         </Popup>
       ) : null}
 
