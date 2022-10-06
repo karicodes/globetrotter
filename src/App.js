4ad946b66d1f4ddbf02a4b314cda161588b97624
Karen Brady <karenbrady@karens-mbp.mynet>
2022-10-06 10:47:01 +0100

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Main from './sections/Main';
 import { Container } from './App.styles';
-import Header from './sections/Header/Header';
-import Footer from './sections/Footer/footer';
 
 function App() {
   return (
